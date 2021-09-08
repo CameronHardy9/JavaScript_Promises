b10b06d66eac5e117c170e213b05388c27a532d7
Cameron Hardy <camhardy99@gmail.com>
2021-09-08 11:45:37 -0500

    exercies done

--- app.js
+++ app.js
@@ -2,3 +2,21 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+
+let watching = new Promise((resolve, reject) => {
+    let userWatchingLiveStream = true;
+    let userLeft = false;
+
+    if(userLeft) {
+        reject({
+            name: "User Left",
+            message: ":("
+        });
+    } else if(userWatchingLiveStream) {
+        resolve("Thumbs up and Subscribe");
+    }
+}) 
+
+watching
+    .then(message => console.log(message))
+    .catch(error => console.log(`${error.name} ${error.message}`));
